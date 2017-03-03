47e60c742e58a08572eff0b01dbfbaacc3dadf2c
Krisztian Groz <krisztian.groz@gmail.com>
2017-03-03 09:49:26 +0000

    updated config

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
 
-const dbURI = process.env.MONGODDB_URI || `mongodb://localhost/PROJECT_NAME-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/PROJECT_NAME-${env}`;
 
 const sessionSecret = process.env.SESSION_SECRET || 'my awesome secret';
 
